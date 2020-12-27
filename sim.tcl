e048dc8ee64c7307db3f12e01d3e34d4da323f9d
Swaraj Hota <swarajhota353@gmail.com>
2020-12-27 21:45:42 +0530

    sim.tcl: drive enable

--- sim.tcl
+++ sim.tcl
@@ -1,5 +1,5 @@
 add_force CLK {0} {1 500ps} -repeat_every 1ns
-add_force EN {1} -cancel_after 2us
+add_force EN {0} {1 1000ns} {0 1500ns}
 
 open_vcd sim.vcd
 log_vcd -level 3 [get_objects CLK EN Q]
